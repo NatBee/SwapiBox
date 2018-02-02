e9b6d427d3d45bc8175cc6cec34f5d91f04466e5
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-01 18:51:38 -0700

    attempt set up api testing suite

--- src/helper.test.js
+++ src/helper.test.js
@@ -2,31 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import helper from './helper';
 import { shallow } from 'enzyme';
+import mockData from './mockData';
+import cleanMockData from '/cleanMockData'
 /* eslint-disable */ 
+console.log(mockData)
+describe('movieData', () => {
+  let mockMovieData;
+  let cleanedMockMovieData;
 
-describe('helper tests', () => {
-  helper = new helper();
-
-  beforeEach(() => {
+  beforeAll(() => {
+    mockMovieData = {mockData}
     renderedComponent = shallow(<App />)
   })
 
-  it('', () => {
+  it('fetch is called with the correct params', () => {
+    const expectedParams = [
+      "https://swapi.co/api/films/",
+      {
+        body: JSON.stringify({films: mockMovieData}),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    ]
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve({
+          movieData: mockMovieData
+        })
+      })
+    })
+  
+
   });
 
-  it('', () => {
+
   
-  })
-
-  it('', () => {
-  })
-
-  it('', () => {
-  })
-
-  it('', () => {
-  })
-
-  it('', () => {
-  })
 })
